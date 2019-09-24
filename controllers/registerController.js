42a2d2fe82760d2d40410d3a6ff2e6189059facf
amoghwagh <amoghwagh@gmail.com>
2019-09-24 19:40:57 +0530

    Wrote a query for registering

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,10 +1,28 @@
+const bcrypt = require('bcrypt');
+const sqlConnection = require('../config/connection');
+
 function register(req, res) {
   const user = {
     user_name: req.body.uname,
     password: req.body.passwd,
     email: req.body.email
   };
-  console.log(user);
+  const salt = bcrypt.genSaltSync(10);
+  const hashedPass = bcrypt.hashSync(user.password, salt);
+  user.password = hashedPass;
+
+  sqlConnection.query(
+    'INSERT INTO users(user_name,password,email) VALUES (?,?,?)',
+    Object.values(user),
+    (err, results) => {
+      if (results.affectedRows === 0) {
+        res.status(404);
+        alert('REGISTER UNSUCCESSFUL!!');
+      } else {
+        alert('REGISTERED SUCCESSFULLY!!');
+      }
+    }
+  );
 }
 
 module.exports = {
