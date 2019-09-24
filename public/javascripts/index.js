00f4eebf62692edb0cf36962f0fb9917332a21af
amoghwagh <amoghwagh@gmail.com>
2019-09-24 20:30:08 +0530

    Made some minor visual changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,5 @@
 if (window.location.pathname === '/') {
   $('#nav-mobile').append('<li><a href="./register">REGISTER</a></li>');
+} else {
+  $('#nav-mobile').append('<li><a href="./">HOME</a></li>');
 }
