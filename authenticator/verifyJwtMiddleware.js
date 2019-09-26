aa59a5cae7769731b6e75257a9b73097ca6a11a2
amoghwagh <amoghwagh@gmail.com>
2019-09-26 20:14:49 +0530

    Added Environment Variables

--- authenticator/verifyJwtMiddleware.js
+++ authenticator/verifyJwtMiddleware.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
 const createError = require('http-errors');
 
 function verifyToken(req, res, next) {
   try {
-    jwt.verify(req.cookies.jwt, 'NotASecretAnymore');
+    jwt.verify(req.cookies.jwt, process.env.SECRET_KEY);
     next();
   } catch (err) {
     next(createError(403, 'You are not Authorized to access'));
--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'rapido'
-});
+const connectionObj = {
+  host: process.env.HOST,
+  user: process.env.SQLUSER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
+};
 
+const connection = mysql.createConnection(connectionObj);
 connection.connect();
 
 module.exports = connection;
